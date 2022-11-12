6e2cfe40ae83c12084d025acf8cced3cb78fdcb9
unknown <alexfung1234@gmail.com>
2022-11-12 21:43:35 +0800

    css and more fetch

--- src/component/top.js
+++ src/component/top.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useState } from "react";
 import { useEffect } from "react";
 
-export default function Top(props) {
+export default function Top() {
 
   const [weather, setWeather] = useState([])
 
@@ -12,12 +12,34 @@ export default function Top(props) {
       .then(data => setWeather(data))
   }, [])
 
+  const [weather2, setWeather2] = useState([])
+
+  useEffect(() => {
+    fetch("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=tc")
+      .then(res => res.json())
+      .then(data => setWeather2(data))
+  }, [])
+
+  console.log(weather2)
 
 
-  return <div>
-    <h1 className="Today">今天天氣</h1>
-    <h2 className="weather">{weather.generalSituation}</h2>
-    <h1 className="future">近日天氣</h1>
-    <h2 className="future-weather">{weather.forecastDesc}</h2>
+  return <div className="main">
+    <div className="top">
+      <div className="todays">
+        <h1 className="title">現時天氣</h1>
+        <h2>{weather.generalSituation}</h2></div>
+      <div className="tmr">
+        <h1 className="title">明日天氣</h1>
+        <h2>{weather.forecastDesc}</h2></div>
+      <div className="future">
+        <h1 className="title">未來幾日天氣</h1>
+        <h2>{weather2.generalSituation}</h2></div>
+    </div>
+
+
+    <div className="updateTime">
+      <h1 className="time-title">更新時間</h1>
+      <h2>{weather.updateTime}</h2></div>
   </div>
+
 }
