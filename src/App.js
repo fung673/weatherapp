6b2c7c88efe4b512014f7bd23c99e12856e8e810
unknown <alexfung1234@gmail.com>
2022-11-20 16:05:37 +0800

    better verison

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ export default function App() {
   return (
     <div>
       <Warn />
-      <Righttop />
       <Top />
     </div>
   )
--- src/component/warn.js
+++ src/component/warn.js
@@ -1,15 +1,55 @@
 import react from 'react'
 import { useState } from "react";
 import { useEffect } from "react";
-import '../images/homepage.png'
+import '../images/temp.png'
 import '../images/warn.png'
 
 export default function Warn() {
+ const [weather3, setWeather3] = useState([])
+ const [box, setBox] = useState(true)
+
+ useEffect(() => {
+  fetch("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc")
+   .then(res => res.json())
+   .then(data => setWeather3(data))
+ }, [])
+
+ function handleClick1() {
+  if (box) {
+   setBox(false)
+  }
+ }
+
+ function handleClick() {
+  if (!box) {
+   setBox(true)
+  }
+ }
+
+ const com = weather3.humidity
+ const humidity = com?.data[0].value
+ const com2 = weather3.temperature
+ const temp = com2?.data[22].value
+
+
  return <div className='navbar'>
   <ul className='nodot'>
-   <a className='home' href='https://fung673.github.io/weatherapp/'><img src={require('../images/homepage.png')} className='homepng' /> 主頁</a>
-   <a className='warn'><img src={require('../images/warn.png')} className='warnpng' /> 特別天氣警報</a>
-  </ul></div>
+   <a className='home' onClick={handleClick}><img src={require('../images/temp.png')} className='homepng' /> 溫度及濕度</a>
+   <a className='warn' onClick={handleClick1}><img src={require('../images/warn.png')} className='warnpng' /> 特別天氣警報</a>
+  </ul>
+  {box ? <div className='right-top-main'>
+   <div className='gradient-border' id="box">
+    <div className='humi'>現時濕度: {humidity}%</div>
+    <div className='temp'>現時氣溫: {temp}°C</div>
+   </div></div> :
+   <div className='right-top-main2'>
+    <div className='gradient-border' id="box2">
+     <div>現時沒有特殊天氣警報</div>
+    </div>
+   </div>}
+
+
+ </div>
 
 
 
